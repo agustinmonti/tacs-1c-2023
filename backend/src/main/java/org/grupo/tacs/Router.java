aade1f3b4feb1a640664b06a5eda4d10c5a98270
JuliaMarti <juulimarti@gmail.com>
2023-04-18 09:59:04 -0300

    localhost:5173

--- backend/src/main/java/org/grupo/tacs/Router.java
+++ backend/src/main/java/org/grupo/tacs/Router.java
@@ -21,7 +21,7 @@ public class Router {
     public static void config(){
         before((request, response) -> {
             response.status(200);
-            response.header("Access-Control-Allow-Origin", "http://localhost:5174");
+            response.header("Access-Control-Allow-Origin", "http://localhost:5173");
             response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
             response.type("application/json");
