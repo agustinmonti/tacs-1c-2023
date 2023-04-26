b58516a9a4604618850377723a6139b0e43112fb
bgabitobrodsky <bernardogabito@gmail.com>
2023-04-26 15:59:11 -0300

    added maquetado of eventList

--- frontend/src/events/components/EventsList.jsx
+++ frontend/src/events/components/EventsList.jsx
@@ -1,17 +1,55 @@
+import { Link } from "react-router-dom"
+import './eventList.css'
 
 export const EventsList = ({ events = [] }) => {
 
     console.log(events)
 
+    const statusClass = ( event ) => {
+        if( event.status === 'Activo'){
+            return 'badge text-bg-info'
+        }
+
+        return 'badge text-bg-secondary'
+    }
+
+    const descriptionText = ( event ) => {
+        if( event.description.length > 75 ){
+            return event.description.slice(0,50) + '...';
+        }
+
+        return event.description;
+    }
+
     return (
         <div>
             {
                 events.map( ev => (
-                    <div key={ ev.id } className="border rounded p-3 mb-2">
-                        <p>{ev.name}</p>
-                        <p>{ev.description}</p>
-                        <p>{ev.status}</p>
-                        <p>{ev.totalParticipants}</p>
+                    <div key={ ev.id } 
+                        className="border rounded mb-2 d-flex justify-content-between align-items-center"
+                        style={{height:'115px'}}
+                    >
+                        <div className="p-3">
+                            <p className="fs-4 m-0">{ev.name}</p>
+                            <p className="fs-6 m-0">{descriptionText(ev)}</p>
+                            <span className={statusClass(ev)}>{ ev.status }</span>
+                        </div>
+                        <div className="d-flex">
+                            <button 
+                                className="btn btn-success px-4 rounded-0"
+                            >
+                                <i class="fa-solid fa-user"></i> 
+                                <br />
+                                {ev.totalParticipants}
+                            </button>
+                            <Link 
+                                to={`/event/${ev.id}`} 
+                                style={{height:'115px',borderStartStartRadius:0,borderEndStartRadius:0}}
+                                className="btn btn-primary px-4 d-flex align-items-center"
+                            >
+                                <i class="fa-solid fa-eye"></i>
+                            </Link>
+                        </div>
                     </div>
                 ))
             }
