3567d14d1d3096ea5d71d291a14054db1cbe2f8d
bgabitobrodsky <bernardogabito@gmail.com>
2023-05-02 19:34:12 -0300

    added "inicio" option to navbar & collapse

--- frontend/src/events/components/Navbar.jsx
+++ frontend/src/events/components/Navbar.jsx
@@ -7,18 +7,27 @@ export const Navbar = () => {
     const { user, startLogout } = useAuthStore();
 
     return (
-        <nav className="navbar bg-primary" data-bs-theme="dark">
-            <div className="container-fluid justify-content-between">
+        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div className="container justify-content-between">
                 
                 <Link className="navbar-brand" to={'/'}>Events-Organizer</Link>
                 
-                <button
-                    className='btn btn-danger'
-                    onClick={ startLogout }
-                >
-                    <i className="fas fa-sign-out-alt"></i>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/'}>Inicio</Link>
+                        </li>                    
+                    </ul>
+                    <button
+                        className='btn btn-danger'
+                        onClick={ startLogout }
+                    >
+                        <i className="fas fa-sign-out-alt"></i>
+                    </button>
+                </div>
             </div>
         </nav>
     )
